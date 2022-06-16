4c3d5fdb08e8f7c1833bfc106cf4abd5d13e07d1
Charalampos Kontos <80924033+ckontos82@users.noreply.github.com>
2022-06-17 01:08:17 +0300

    Add files via upload
    Α simple, command line tic-tac-toe game, written in C.
    
    If you want to compile it in Windows, change "clear" in line 15 with "cls".

---
+++ tic.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIM 3
+#define RND 9
+
+void putChoice(int, char [][3], int);
+void display(char [DIM][DIM]);
+void status(char [DIM][DIM], int);
+int isValid(char [DIM][DIM], int);
+
+
+int main(void)
+{
+    char board[DIM][DIM];
+    int player = 1;
+    int choice;
+    int  round = 0;
+    char stream[10], *endptr;
+    
+    for (int i = 0; i < DIM; i++)
+        for (int j = 0; j < DIM; j++)
+            board[i][j] = 3 * i + j + 1 + '0';
+
+    system("clear");
+    display(board);
+
+    printf("\n\nPlayer 1 has (X), player 2 has (O).\n\n");
+    do {       
+        ++round;
+        if (player % 2 == 0)
+            player = 2;
+        else
+            player = 1;
+        
+        do{
+            printf("\n\nPlayer %d is playing. Enter your choice: ", player);
+            fgets(stream, 9, stdin);
+            choice = strtol(stream, &endptr, 10);
+        } while ((choice < 1) || (choice > 9) || (!isValid(board, choice)));
+
+        putChoice(player, board, choice);
+        display(board);
+        status(board, player);
+        ++player;
+    } while (round < RND);
+
+    printf("\n\nGame is a tie.\n\n");
+    return 0;
+}
+
+void display(char array[DIM][DIM])
+{
+    for (int i = 0; i < DIM; i++)
+    {
+        if (i != 0)
+            printf("\n-----\n");
+        for (int j = 0; j < DIM; j++)
+        {
+            printf("%c", array[i][j]);
+            if (j != 2)
+                printf("|");
+        }
+    }
+}
+
+void putChoice(int p, char array[][DIM], int choice)
+{
+    int i = (choice - 1) / DIM;
+    int j = (choice - 1) % DIM;
+    switch (p)
+    {
+    case 1:
+        array[i][j] = 'X';
+        break;
+    
+    case 2:
+        array[i][j] = 'O';
+        break;
+    }
+}
+
+void status(char array[DIM][DIM], int p)
+{
+    int i;
+    for (i = 0; i < DIM; i++)
+    {
+        if ((array[i][0] == array[i][1]) && (array[i][1] == array[i][2]))
+        {
+            printf("\n\nPlayer %d has won the game.\n");
+            exit(0);
+        }    
+        if ((array[0][i] == array[1][i]) && (array[1][i] == array[2][i]))
+        {
+            printf("\n\nPlayer %d has won the game.\n");
+            exit(0);
+        }
+    }
+
+    if ((array[0][0] == array[1][1]) && (array[1][1] == array[2][2]))
+    {
+            printf("\n\nPlayer %d has won the game.\n");
+            exit(0);
+    }
+
+    if ((array[0][2] == array[1][1]) && (array[1][1] == array[2][0]))
+    {
+            printf("\n\nPlayer %d has won the game.\n");
+            exit(0);
+    }
+}
+
+int isValid(char array[DIM][DIM], int c)
+{
+    int i = (c- 1) / DIM;
+    int j = (c - 1) % DIM;
+
+    if ((array[i][j] == 'X') || (array[i][j] == 'O'))
+        return 0;
+
+    return 1;
+}
